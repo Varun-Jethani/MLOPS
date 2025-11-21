5bced48a4bbe8aae6b54622d36c596d44be1e195
Varun <varunjethani@outlook.com>
2025-11-22 01:17:25 +0530

    almost

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'youtube_sentiment',
+    version= '0.0.0',
+    author= 'Varun',
+    author_email= 'varunjethani2444@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
